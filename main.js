ee4833f091f7f2132b509ea7b28d5c8a9d1a41af
hanabotz <92577238+hanabotz@users.noreply.github.com>
2022-03-24 07:46:30 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ const setting = JSON.parse(fs.readFileSync('./setting.json'))
 
 const welcome = require('./message/group')
 
-
+const ftroli ={key: {fromMe: false,"participant":"0@s.whatsapp.net",   "remoteJid": "6289523258649-1604595598@g.us"  }, "message": {orderMessage: {itemCount: 2021,status: 200, thumbnail: fs.readFileSync(`media/odc.jpeg`), surface: 200, message: `ʀɪғǫɪ ʙᴏᴛᴢ`, orderTitle: `RIFQIBOTZ`, sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
 
 baterai = 'unknown'
 
@@ -133,7 +133,7 @@ const starts = async (ikyy = new WAConnection()) => {
 
 	ikyy.logger.level = 'warn'
 
-ikyy.version = [2, 2147, 14];
+ikyy.version = [2, 2149, 4]
 
 	console.log(color(figlet.textSync('YT RIFQI BOTZ', {
 
@@ -222,35 +222,29 @@ console.log(color(`${spc4}< ================================================== >
 
 console.log(color('[ LORD RIFQI BOTZ ]', 'cyan'), color('Bot Sudah Online!'));
 
-ikyy.browserDescription = ["RIFQI - BOTZ", "Firefox", "3.0.0"];
+ikyy.browserDescription = ["RIFQI - BOTZ", "Safari", "3.0.0"];
 
 
 	})
 
+//join ke gc
 
-/**join ke gc
-
-teks = `https://chat.whatsapp.com/Hnb8UOsuN4t5hSEMNTUdft`
+/**teks = `https://chat.whatsapp.com/JtXKjv1uGhKJyrpERmN6AZ`
  ikyy.query({ json:["action", "invite", `${teks.replace('https://chat.whatsapp.com/','')}`]})
  console.log(color('|INFO!|', 'yellow'), color('JOINED TO FORUM WHATSAPP BOT GROUP', 'cyan'))**/
 
 
 // send message 
-/**ikyy.sendMessage(`6289636634511@s.whatsapp.net`, `*Bot On Mek, Tersambung Pada Nomor Ini*\n────────────────────\n\`\`\`${JSON.stringify(ikyy.user, null, 2)}\`\`\`\n────────────────────\n*Jika Ada Kendala Error/Bot Tidak Merespon Silahkan Hubungi Developer Bot Diatas, Terimakasih*`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Owner RIFQI BOTZ",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./connect.jpg'),sourceUrl:"https://wa.me/6289636634511?text=Assalamualaikum"}}})
 
-console.log(color('|INFO!|', 'yellow'), color('mengirim info bot ke owner', 'cyan'))
-
-fetch(`http://ip-api.com/line`).then(res => res.text())  
+/**fetch(`http://ip-api.com/line`).then(res => res.text())  
 
         .then(bu =>{
 
-       ikyy.sendMessage("6289636634511@s.whatsapp.net", `─────「 *IP-USER* 」─────\n\n\`\`\`${bu}\`\`\`\n────────────────────`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Owner RIFQI BOTZ",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./connect.jpg'),sourceUrl:"https://wa.me/6289636634511?text=Assalamualaikum"}}})
+       ikyy.sendMessage("0@s.whatsapp.net", `halo mark🗿`, MessageType.text, {contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title: "Owner RIFQI BOTZ",body:"",previewType:"PHOTO",thumbnail:fs.readFileSync('./connect.jpg'),sourceUrl:"https://wa.me/0?text=Assalamualaikum"}}})
 
      console.log(color('|INFO!|', 'yellow'), color('mengirim info bot ke owner', 'cyan'))
 
    })**/
-
-
 	// session
 
 
@@ -269,18 +263,9 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
 
 	fs.writeFileSync(`./${setting.sessionName}.json`, JSON.stringify(ikyy.base64EncodedAuthInfo(), null, '\t'))
 
+//jd
 
-
-
-        
-        
-        
-
-        //
-        /*=======================================================*/
-
-				
-				if (hour_now >= '02:00' && hour_now <= '04:00') {
+if (hour_now >= '02:00' && hour_now <= '04:00') {
           console.log(color('[PESAN DARI ORANG BAIK]', 'cyan'), color('Waktunya sahur kak, Main botnya buat nanti lagi, Sebelum makan jangan lupa baca Doa ya kak', 'yellow'), color('(😊)', 'white'))
           }
         if (hour_now >= '04:00' && hour_now <= '05:00') {
@@ -320,6 +305,13 @@ fetch(`http://ip-api.com/line`).then(res => res.text())
            console.log(color('[PESAN DARI ORANG BAIK]', 'cyan'), color('KenzyBot ngantuk kak, tidur dulu ya kak', 'yellow'), color('(😴)', 'white'))
         }
         
+        
+        
+
+        //
+        /*=======================================================*/
+
+				
 	// Baterai
 
 
@@ -434,7 +426,7 @@ ikyy.on('CB:action,,call', async json => {
 
 
 
-/**antidel = false
+antidel = false
 ikyy.on('message-delete', async (m) => {
 if (m.key.remoteJid == 'status@broadcast') return
 if (!m.key.fromMe && m.key.fromMe) return
@@ -460,7 +452,7 @@ ikyy.sendMessage(m.key.remoteJid, `\`\`\` A N T I  D E L E T E \`\`\`
 ˊ Type : ${type}`, MessageType.text, {quoted: m.message, contextInfo: {"mentionedJid": [m.participant]}})
 
 ikyy.copyNForward(m.key.remoteJid, m.message)
-})**/
+})
 
 
   
@@ -488,4 +480,3 @@ ikyy.copyNForward(m.key.remoteJid, m.message)
 
 
 starts()
-
